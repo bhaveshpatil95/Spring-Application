e6f1cfc57f61c3e0de1bf14b49cb0ab76d73cb4f
Bhaveshpatil95 <bhaveshpatil3987@gmail.com>
2023-12-14 10:15:40 +0530

    new file

--- Java.java
+++ Java.java
@@ -1,7 +1,8 @@
-public class Addition{
+public class Java{
 
 public static void main(String[] args){
 
+System.out.println(c);
 
 }
 
---
+++ code.java
@@ -0,0 +1,12 @@
+public class Addition{
+
+public static void main(String[] args){
+
+int a=20;
+int b=40;
+int c=a+b;
+System.out.println(c);
+
+}
+
+}
